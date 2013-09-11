59a9fcd9be796e5b9ed44bfb846c9365d42f0fe2
Sam Fishman <sfishman@college.harvard.edu>
2013-09-11 02:17:19 -0400

    finished price-changing feature

--- js/reg.js
+++ js/reg.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	var $iframe = $('iframe');
 
-	$iframe.ready(function){
+	$iframe.load(function(){
 		$iframe.contents().find('input[name="entry.580305412"], input[name="returning_students"], input[name="entry.1881152076"]').change(function(){
 			updatePrice();
 		});
@@ -13,8 +13,11 @@ function updatePrice() {
 
 	var price = 30; // Base price is $30
 
-	var tot_students = parseInt($iframe.find('input[name="entry.580305412"]').val());
-	var ret_students = parseInt($iframe.find('input[name="returning_students"]').val());
+	var totval = parseInt($iframe.find('input[name="entry.580305412"]').val());
+	var retval = parseInt($iframe.find('input[name="returning_students"]').val());
+
+	var tot_students = isNaN(totval) ? 0 : totval;
+	var ret_students = isNaN(retval) ? 0 : retval;
 	var review = $iframe.find('input[name="entry.1881152076"]:checked').val() == 'Yes';
 
 	price += (tot_students - ret_students) * 65;
@@ -24,4 +27,5 @@ function updatePrice() {
 	}
 
 	$('div#pricebox').html('$' + price);
+	$('form[name="_xclick"] > input[name="amount"]').val(price);
 }
